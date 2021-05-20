cf95557646232a34a78613d8476c102750b35184
Justin Welch <justin.welch075@gmail.com>
2021-05-20 17:55:12 -0500

    added function to view employees by role

--- server.js
+++ server.js
@@ -109,8 +109,79 @@ function viewAllEmp(){
 }
 
 //view employees by department
+function viewAllEmpByDept(){
+    let deptArr = [];
+
+    promisemysql.createConnection(connectionProperties
+    ).then((conn) => {
+
+        return conn.query('SELECT name FROM department');
+    }).then(function(value){
+
+        deptQuery = value;
+        for (i=0; i < value.length; i++){
+            deptArr.push(value[i].name);
+            
+        }
+    }).then(() => {
+        inquirer.prompt({
+            name: "department",
+            type: "list",
+            message: "Please select a department.",
+            choices: deptArr
+        })    
+        .then((answer) => {
+            const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = '${answer.department}' ORDER BY ID ASC`;
+            connection.query(query, (err, res) => {
+                if(err) return err;
+
+                console.log("\n");
+                console.table(res);
+
+                mainMenu();
+            });
+        });
+    });
+}
+
 
 //view all employees by role 
+function viewAllEmpByRole(){
+
+    let roleArr = [];
+
+    promisemysql.createConnection(connectionProperties)
+    .then((conn) => {
+
+        return conn.query('SELECT title FROM role');
+    }).then(function(roles){
+
+        for (i=0; i < roles.length; i++){
+            roleArr.push(roles[i].title);
+        }
+    }).then(() => {
+        inquirer.prompt({
+            name: "role",
+            type: "list",
+            message: "Which role would you like to search?",
+            choices: roleArr
+        })    
+        .then((answer) => {
+
+            const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE role.title = '${answer.role}' ORDER BY ID ASC`;
+            connection.query(query, (err, res) => {
+                if(err) return err;
+
+                console.log("\n");
+                console.table(res);
+                
+                mainMenu();
+            });
+        });
+    });
+}
+
+
 
 //adding an employee
 
