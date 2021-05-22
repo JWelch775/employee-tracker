f0debdeb3959cb6353b833ac939803c4d43b2d58
Justin Welch <justin.welch075@gmail.com>
2021-05-21 20:35:41 -0500

    added function to view all employees by manager

--- server.js
+++ server.js
@@ -422,8 +422,117 @@ function updateEmpRole(){
 }
 
 //update employee manager
+function updateEmpMngr(){
+
+    let employeeArr = [];
+
+    promisemysql.createConnection(connectionProperties
+    ).then((conn) => {
+
+        return conn.query("SELECT employee.id, concat(employee.first_name, ' ' ,  employee.last_name) AS Employee FROM employee ORDER BY Employee ASC");
+    }).then((employees) => {
+
+        for (i=0; i < employees.length; i++){
+            employeeArr.push(employees[i].Employee);
+        }
+
+        return employees;
+    }).then((employees) => {
+
+        inquirer.prompt([
+            {
+                name: "employee",
+                type: "list",
+                message: "Who would you like to edit?",
+                choices: employeeArr
+            }, {
+                name: "manager",
+                type: "list",
+                message: "Who is their new Manager?",
+                choices: employeeArr
+            },]).then((answer) => {
+
+                let employeeID;
+                let managerID;
+
+                for (i=0; i < employees.length; i++){
+                    if (answer.manager == employees[i].Employee){
+                        managerID = employees[i].id;
+                    }
+                }
+
+                for (i=0; i < employees.length; i++){
+                    if (answer.employee == employees[i].Employee){
+                        employeeID = employees[i].id;
+                    }
+                }
+
+                connection.query(`UPDATE employee SET manager_id = ${managerID} WHERE id = ${employeeID}`, (err, res) => {
+                    if(err) return err;
+
+                    console.log(`\n ${answer.employee} MANAGER UPDATED TO ${answer.manager}...\n`);
+
+                    mainMenu();
+                });
+            });
+    });
+}
+
 
 //view all employees by manager
+function viewAllEmpByMngr(){
+
+    let managerArr = [];
+
+    promisemysql.createConnection(connectionProperties)
+    .then((conn) => {
+
+        return conn.query("SELECT DISTINCT m.id, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e Inner JOIN employee m ON e.manager_id = m.id");
+
+    }).then(function(managers){
+
+        for (i=0; i < managers.length; i++){
+            managerArr.push(managers[i].manager);
+        }
+
+        return managers;
+    }).then((managers) => {
+
+        inquirer.prompt({
+
+            name: "manager",
+            type: "list",
+            message: "Which manager would you like to search?",
+            choices: managerArr
+        })    
+        .then((answer) => {
+
+            let managerID;
+
+            for (i=0; i < managers.length; i++){
+                if (answer.manager == managers[i].manager){
+                    managerID = managers[i].id;
+                }
+            }
+
+            const query = `SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager
+            FROM employee e
+            LEFT JOIN employee m ON e.manager_id = m.id
+            INNER JOIN role ON e.role_id = role.id
+            INNER JOIN department ON role.department_id = department.id
+            WHERE e.manager_id = ${managerID};`;
+    
+            connection.query(query, (err, res) => {
+                if(err) return err;
+                
+                console.log("\n");
+                console.table(res);
+
+                mainMenu();
+            });
+        });
+    });
+}
 
 //delete an employee
 
